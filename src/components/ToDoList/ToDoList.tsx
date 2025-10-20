f0242247a53a4aae37bf2439fc8619741b9466e0
Viktor <viktor.avgn@yandex.ru>
2025-10-21 00:36:23 +0700

    логика добавления задачи, изменения статуса и удаления задачи

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -1,15 +1,48 @@
 import './ToDoList.scss'
 import { ToDoListItem } from './ToDoListItem/ToDoListItem'
+import { ToDo } from '../../models/todo-item'
 
-export const ToDoList = () => {
+export const ToDoList = (props: {
+  todos: ToDo[]
+  updateToDo: Function
+  deleteToDo: Function
+}) => {
+  const checkedList = () => {
+    return props.todos
+      .filter((item) => !item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoListItem
+            key={idx}
+            toDoItem={item}
+            updateToDo={props.updateToDo}
+            deleteToDo={props.deleteToDo}
+          />
+        )
+      })
+  }
+
+  const unCheckedList = () => {
+    return props.todos
+      .filter((item) => item.isDone)
+      .map((item, idx) => {
+        return (
+          <ToDoListItem
+            key={idx}
+            toDoItem={item}
+            updateToDo={props.updateToDo}
+            deleteToDo={props.deleteToDo}
+          />
+        )
+      })
+  }
+
+  //-------------------------------------------------------------------
+
   return (
     <div className="todo-container">
-      <ul className="todo-list failed">
-        <ToDoListItem />
-      </ul>
-      <ul className="todo-list completed">
-        <ToDoListItem />
-      </ul>
+      <ul className="todo-list failed">{checkedList()}</ul>
+      <ul className="todo-list completed">{unCheckedList()}</ul>
     </div>
   )
 }
